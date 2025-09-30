a0914557fc4a14d0628b6536e2f5c472a4a57a7b
Bert Spaan <bert.spaan@gmail.com>
2025-09-30 10:40:26 +0200

    added ids

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { generateId } from '@allmaps/id'
+
+import sourceProjections from './source/projections.json' with { type: 'json' }
+
+type Projection = {
+  id: string
+  definition: string
+  name: string
+  epsg: number
+}
+
+const projections: Projection[] = []
+
+for (const projection of sourceProjections) {
+  const id = await generateId(projection.definition)
+
+  projections.push({
+    id: `https://api.allmaps.org/projections/${id}`,
+    definition: projection.definition.trim(),
+    name: `EPSG:${projection.code} - ${projection.name}`,
+    epsg: Number(projection.code)
+  })
+}
+
+console.log(JSON.stringify(projections, null, 2))
